e1d03ae0dc9699163ae38008c285122bfa6c5753
MuQiaoYu <1241142935@qq.com>
2023-07-19 16:50:22 +0800

    Initial commit

---
+++ app/src/main/java/com/fish/photoshare/activities/UserActivity.java
@@ -0,0 +1,14 @@
+package com.fish.photoshare.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fish.photoshare.R;
+
+public class UserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
