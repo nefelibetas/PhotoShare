fdd00769246e6adbe23ba323b9fd30015c5d0fce
nefelibetas <1241142935@qq.com>
2023-07-25 21:33:26 +0800

    构建大体框架

---
+++ app/src/main/java/com/fish/photoshare/fragments/HomeFragment.java
@@ -0,0 +1,69 @@
+package com.fish.photoshare.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fish.photoshare.POJO.Post;
+import com.fish.photoshare.R;
+import com.fish.photoshare.adapter.HomeAdapter;
+
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+    private ArrayList<Post> postList;
+
+    public HomeFragment() {}
+
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void mockData(){
+        String[] titles = getResources().getStringArray(R.array.titles);
+        String[] authors = getResources().getStringArray(R.array.authors);
+        String[] images = getResources().getStringArray(R.array.images);
+
+        int len = 0;
+        len = Math.max(titles.length, authors.length);
+        len = Math.max(len, images.length);
+        for (int i = 0; i < len; i++) {
+            String title = titles[i];
+            String author = authors[i];
+            int imageId = Integer.parseInt(images[i]);
+            Post post = new Post(title, author, imageId);
+            postList.add(post);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeAdapter adapter = new HomeAdapter( );
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ app/src/main/java/com/fish/photoshare/utils/ToastUtils.java
@@ -0,0 +1,10 @@
+package com.fish.photoshare.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils {
+    public static void show(Context context , String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
